47425f324338e06de147997f8ab4bb4d0a9df209
JR Lim <josh.r.lim@gmail.com>
2018-02-05 13:43:22 -0600

    Added gem model & card format

--- src/app/gem/gem.component.ts
+++ src/app/gem/gem.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GemModel } from '../../gemmodel';
+
 @Component({
   selector: 'app-gem',
   templateUrl: './gem.component.html',
   styleUrls: ['./gem.component.css']
 })
 export class GemComponent implements OnInit {
+    
+    gem : GemModel = {
+        id: 1,
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'Some gems have hidden qualities beyond their lustre, beyond their shine... Dodecahedron is one of those gems.'
+    }
 
   constructor() { }
 
